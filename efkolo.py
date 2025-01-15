0713ea99803803b08754128eb43ec9b65fe71eb7
Elinapk <116118910+Elinapk@users.noreply.github.com>
2025-01-15 20:15:38 +0100

    Add files via upload

---
+++ efkolo.py
@@ -0,0 +1,26 @@
+first_name = input('jak masz na imie? ')
+age = input('ile masz lat? ')
+print (f'Witaj  {first_name} !')
+print(f'Mam  {age}  lata. ')
+
+
+#twoj_zodiak = input('Jaki masz zodiak? ')
+#rok_urodzenia = input('Podaj rok urodznia? ')
+
+
+#print (f'Moj zodiak to {twoj_zodiak} urodzilam sie w {rok_urodzenia} ')
+
+if first_name == 'Era':
+    print('Czesc Era ')
+else:
+    print('Milo mi Cie poznac!')
+
+if age >= '33':
+    print('Jestes juz old ')
+else:
+    print('Nie jest tak źle ')
+
+#nazwisko_stare = 'Pieta '
+#nazwisko_nowe = 'Palec '
+#imie = 'Era '
+#print('Czesc ' + imie + nazwisko_stare + nazwisko_nowe + 'miło mi Cię poznać')
